8ad6094c3c25cdb1ccc0ddbd00987f1e87715a8d
morozkaanya <anya-moro1112@yandex.ru>
2023-06-21 12:44:45 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <?php
-$name = "не определено";
-$age = "не определен";
-if(isset($_POST["name"])){
-  
-    $name = $_POST["name"];
-}
-if(isset($_POST["age"])){
-  
-    $age = $_POST["age"];
-}
-echo "Имя: $name <br> Возраст: $age";
+echo "Имя: name <br>  $surname";
 ?>
